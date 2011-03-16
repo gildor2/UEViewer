beb0ce3403dedbb1235e782213edc3eea7d3b6f1
Konstantin Nosov <gildor2@users.noreply.github.com>
2011-03-16 17:08:11 +0000

    - fix for the previous commit - lost fixes
    git-svn-id: svn://localhost@125 a3f5f06f-15b6-264c-8744-b656ee826ca1

---
+++ Exporters/ExportSound.cpp
@@ -0,0 +1,71 @@
+#include "Core.h"
+#include "UnCore.h"
+
+#include "UnObject.h"
+#include "UnPackage.h"		// for Package->Name
+#include "UnSound.h"
+
+#include "Exporters.h"
+
+
+static void SaveSound(const UObject *Obj, void *Data, int DataSize, const char *DefExt)
+{
+	// check for enough place for header
+	if (DataSize < 16)
+	{
+		printf("... empty sound %s ?\n", Obj->Name);
+		return;
+	}
+
+	const char *ext = DefExt;
+
+	if (!memcmp(Data, "OggS", 4))
+		ext = "ogg";
+	else if (!memcmp(Data, "RIFF", 4))
+		ext = "wav";
+
+	char filename[512];
+	appSprintf(ARRAY_ARG(filename), "%s/%s.%s", GetExportPath(Obj), Obj->Name, ext);
+	FFileReader Ar2(filename, false);
+
+	Ar2.Serialize(Data, DataSize);
+}
+
+
+void ExportSound(const USound *Snd, FArchive &Ar)
+{
+	SaveSound(Snd, (void*)&Snd->RawData[0], Snd->RawData.Num(), "unk");
+}
+
+
+#if UNREAL3
+
+void ExportSoundNodeWave(const USoundNodeWave *Snd, FArchive &Ar)
+{
+	// select bulk containing data
+	const FByteBulkData *bulk = NULL;
+	const char *ext = "unk";
+
+	if (Snd->RawData.ElementCount)
+	{
+		bulk = &Snd->RawData;
+	}
+	else if (Snd->CompressedPCData.ElementCount)
+	{
+		bulk = &Snd->CompressedPCData;
+	}
+	else if (Snd->CompressedXbox360Data.ElementCount)
+	{
+		bulk = &Snd->CompressedXbox360Data;
+		ext  = "x360audio";
+	}
+	else if (Snd->CompressedPS3Data.ElementCount)
+	{
+		bulk = &Snd->CompressedPS3Data;
+		ext  = "ps3audio";
+	}
+
+	SaveSound(Snd, bulk->BulkData, bulk->ElementCount, ext);
+}
+
+#endif // UNREAL3
