35f72891999733d0eb6517132e4c0d1c8c4f65bc
Konstantin Nosov <git@gildor.org>
2014-09-28 17:57:00 +0400

    Moved all GAME_XXX defines to GameDefines.h, used by umodel, extract and decompress tools (so, these tools are now supports all games which are supported by umodel).

--- Tools/PackageUnpack/Build.h
+++ Tools/PackageUnpack/Build.h
@@ -1,46 +1,4 @@
 #define DO_GUARD		1
 
-#define UNREAL1			1
-#define UNREAL25		1
-#define UNREAL3			1
-
-// UE2 (supported by default)
-#define UT2				1
-#define PARIAH			1
-#define SPLINTER_CELL	1
-#define LINEAGE2		1
-#define BATTLE_TERR		1		// Battle Territory Online
-
-// requires UNREAL25
-#define TRIBES3			1
-//#define RAGNAROK2		1
-#define EXTEEL			1
-
-#define UC2				1
-
-// requires UNREAL3
-#define XBOX360			1
-#define BIOSHOCK		1
-#define MASSEFF			1
-#define HUXLEY			1
-
-#define BATMAN			1
-#define AVA				1		// AVA Online
-#define A51				1		// Blacksite: Area 51
-#define WHEELMAN		1		//?? incomplete
-#define MKVSDC			1		// Mortal Kombat vs. DC Universe
-#define STRANGLE		1		// Stranglehold
-#define TNA_IMPACT		1		// TNA iMPACT!
-#define TERA			1
-#define BLADENSOUL		1		// Blade & Soul
-#define APB				1
-#define ALPHA_PR		1
-#define TRANSFORMERS	1
-#define DCU_ONLINE		1		// DC Universe Online
-#define BULLETSTORM		1
-#define NURIEN			1
-#define HAWKEN			1
-#define ALIENS_CM		1
-#define BIOSHOCK3		1		// Bioshock Infinite
-#define REMEMBER_ME		1
-
+// Use all supported games
+#include "GameDefines.h"
--- Tools/TypeInfo/Build.h
+++ Tools/TypeInfo/Build.h
@@ -1,39 +1,4 @@
 #define DO_GUARD		1
 
-#define UNREAL1			1
-#define UNREAL25		1
-#define UNREAL3			1
-
-// UE2 (supported by default)
-#define UT2				1
-#define PARIAH			1
-#define SPLINTER_CELL	1
-#define LINEAGE2		1
-
-// requires UNREAL25
-#define TRIBES3			1
-//#define RAGNAROK2		1
-#define EXTEEL			1
-
-#define UC2				1
-
-// requires UNREAL3
-#define XBOX360			1
-#define BIOSHOCK		1		//!! requires UNREAL3 and TRIBES3
-#define MASSEFF			1
-#define HUXLEY			1
-#define BORDERLANDS		1
-#define ALIENS_CM		1
-
-#define A51				1		// Blacksite: Area 51
-#define WHEELMAN		1		//?? incomplete
-#define MKVSDC			1		// Mortal Kombat vs. DC Universe
-#define STRANGLE		1		// Stranglehold
-#define TNA_IMPACT		1		// TNA iMPACT!
-#define APB				1
-#define TRANSFORMERS	1
-#define DCU_ONLINE		1		// DC Universe Online
-#define BATMAN			1
-#define BIOSHOCK3		1		// Bioshock Infinite
-#define LOST_PLANET3	1
-#define THIEF4			1
+// Use all supported games
+#include "GameDefines.h"
