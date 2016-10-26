3e4cda3f29ddad80d6864ecdb66a611c23841d8d
Konstantin Nosov <git@gildor.org>
2016-10-26 13:17:45 +0300

    Moved USkeleton stuff to UnAnim4.cpp

--- Unreal/UnAnim4.cpp
+++ Unreal/UnAnim4.cpp
@@ -6,6 +6,61 @@
 #include "UnMesh4.h"
 #include "UnMeshTypes.h"
 
+/*-----------------------------------------------------------------------------
+	USkeleton
+-----------------------------------------------------------------------------*/
+
+FArchive& operator<<(FArchive& Ar, FMeshBoneInfo& B)
+{
+	Ar << B.Name << B.ParentIndex;
+	if (Ar.ArVer < VER_UE4_REFERENCE_SKELETON_REFACTOR)
+	{
+		FColor Color;
+		Ar << Color;
+	}
+	// Support for editor packages
+	if ((Ar.ArVer >= VER_UE4_STORE_BONE_EXPORT_NAMES) && Ar.ContainsEditorData())
+	{
+		FString ExportName;
+		Ar << ExportName;
+	}
+	return Ar;
+}
+
+void USkeleton::Serialize(FArchive &Ar)
+{
+	guard(USkeleton::Serialize);
+
+	Super::Serialize(Ar);
+
+	if (Ar.ArVer >= VER_UE4_REFERENCE_SKELETON_REFACTOR)
+		Ar << ReferenceSkeleton;
+
+	if (Ar.ArVer >= VER_UE4_FIX_ANIMATIONBASEPOSE_SERIALIZATION)
+	{
+		Ar << AnimRetargetSources;
+	}
+	else
+	{
+		appPrintf("USkeleton has old AnimRetargetSources format, skipping\n");
+		DROP_REMAINING_DATA(Ar);
+		return;
+	}
+
+	if (Ar.ArVer >= VER_UE4_SKELETON_GUID_SERIALIZATION)
+		Ar << Guid;
+
+	if (Ar.ArVer >= VER_UE4_SKELETON_ADD_SMARTNAMES)
+		Ar << SmartNames;
+
+	unguard;
+}
+
+
+/*-----------------------------------------------------------------------------
+	UAnimSequence
+-----------------------------------------------------------------------------*/
+
 void UAnimSequence4::Serialize(FArchive& Ar)
 {
 	guard(UAnimSequence4::Serialize);
