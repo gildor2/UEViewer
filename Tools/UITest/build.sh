0daa60756e87bccdae45ea2ba809173cfb334665
Konstantin Nosov <git@gildor.org>
2019-07-09 10:22:02 +0300

    Updated VS Code project
    - settings.json: included some Tools projects into file list
    - launch.json: added separate option for UITest
    - tasks.json: using built-in problem matcher, added task for building UITest
    - UITest/build.sh: passing command line parameters to main build.sh

--- Tools/UITest/build.sh
+++ Tools/UITest/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# VC2013 has problems with using lambdas in BIND_LAMBDA, so use VS2015
-vc_ver=2015
+vc_ver=latest
 project="uitest"
 root="../.."
 render=0
-source $root/build.sh
+source $root/build.sh $*
