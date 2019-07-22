d31d91494e18d41209b70969da33242eda735bb7
Konstantin Nosov <git@gildor.org>
2019-07-22 22:46:49 +0300

    Fixed SMITE and Paladins mesh loading
    Were broken due to changes from 2019-Jun-22 (9c1a25d)

--- Unreal/GameDatabase.cpp
+++ Unreal/GameDatabase.cpp
@@ -906,6 +906,9 @@ static const UEVersionMap ueVersions[] =
 #if DND
 	G(GAME_DND, 673)						// real version is 674
 #endif
+#if SMITE
+	G(GAME_Smite, 868)						// Paladins, Smite: real version is 893; version 869 has changes in mesh format
+#endif
 	G(GAME_GoWJ, 828)						// real version is 846
 };
 
