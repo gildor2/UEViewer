gildor2/UEViewer

commit 9c1a25d1f1bb5c9a3f71f27c911d7396bc7576f4

Added support for newer UE3 StaticMesh data

Tools/CompatTable/table.ini
@@ -514,10 +514,10 @@ NetherRealm Studios
 http://en.wikipedia.org/wiki/Injustice_2
 https://www.gildor.org/smf/index.php/topic,3202.0.html
 
-[Kingdom Hearts 0.2: Birth by Sleep – A Fragmentary Passage]
+[Kingdom Hearts 0.2: Birth by Sleep ï¿½ A Fragmentary Passage]
 Y Y Y Y UE4.12
 Square Enix
-https://en.wikipedia.org/wiki/Kingdom_Hearts_HD_2.8_Final_Chapter_Prologue#Kingdom_Hearts_0.2:_Birth_by_Sleep_–_A_Fragmentary_Passage
+https://en.wikipedia.org/wiki/Kingdom_Hearts_HD_2.8_Final_Chapter_Prologue#Kingdom_Hearts_0.2:_Birth_by_Sleep_ï¿½_A_Fragmentary_Passage
 https://www.gildor.org/smf/index.php/topic,5999.0.html
 
 [Kings and Heroes]
@@ -927,6 +927,12 @@ Y Y Y Y 867.8
 Splash Damage
 http://en.wikipedia.org/wiki/Dirty_Bomb_(video_game)
 https://www.gildor.org/smf/index.php/topic,2571.0.html
+
+[GRAV (alpha)]
+Y Y Y Y 869.5
+BitMonster
+https://store.steampowered.com/app/332500/GRAV/
+https://www.gildor.org/smf/index.php/topic,2943.0.html
 
 [King's Quest]
 Y Y Y Y ?

Unreal/UnMesh3.cpp
@@ -2399,6 +2399,12 @@ struct FStaticMeshSection3
 					ps3data.unk5.Num(), ps3data.unk6.Num(), ps3data.unk7.Num(), ps3data.unk8.Num());
 			}
 		}
+		if (Ar.ArVer >= 869)
+		{
+			// Picked from GRAV executable
+			int32 unk;
+			Ar << unk;
+		}
 #if XCOM
 		if (Ar.Game == GAME_XcomB)
 		{
@@ -2407,7 +2413,7 @@ struct FStaticMeshSection3
 		}
 		if (Ar.Game == GAME_Xcom2 && Ar.ArLicenseeVer >= 83)
 		{
-			int unk;
+			int32 unk;
 			Ar << unk;
 		}
 #endif // XCOM

commit 1494f6978b2e9cf095ad97783a7aa45c07ff4455

Fixed incompatibility with some UE3 games
It was caused by commit 9c1a25d (changes made for GRAV). Put these changes under GAME_GRAV.

Tools/CompatTable/table.ini
@@ -998,6 +998,7 @@ Y Y Y Y 869.5
 BitMonster
 https://store.steampowered.com/app/332500/GRAV/
 https://www.gildor.org/smf/index.php/topic,2943.0.html
+; -game=grav
 
 [King's Quest]
 Y Y Y Y ?

Unreal/GameDatabase.cpp
@@ -386,6 +386,9 @@ const GameInfo GListOfGames[] = {
 #	if LIS2
 		G("Life is Strange 2", lis2, GAME_LIS2),
 #	endif
+#	if GRAV
+		G("GRAV", grav, GAME_GRAV),
+#	endif
 #endif // UNREAL4
 
 	// end marker
@@ -905,9 +908,6 @@ static const UEVersionMap ueVersions[] =
 #endif
 #if DND
 	G(GAME_DND, 673)						// real version is 674
-#endif
-#if SMITE
-	G(GAME_Smite, 868)						// Paladins, Smite: real version is 893; version 869 has changes in mesh format
 #endif
 	G(GAME_GoWJ, 828)						// real version is 846
 };

Unreal/GameDefines.h
@@ -132,6 +132,7 @@
 #define DUNDEF			1		// Dungeon Defenders
 #define DEVILS_THIRD	1		// Devil's Third
 #define ROCKET_LEAGUE	1		// Rocket League
+#define GRAV			1
 //#define USE_XDK			1		// use some proprietary code for XBox360 support
 
 // Midway UE3 games -- make common define ??

Unreal/UnCore.h
@@ -402,6 +402,7 @@ enum EGame
 		GAME_Smite,
 		GAME_DevilsThird,
 		GAME_RocketLeague,
+		GAME_GRAV,
 
 	GAME_MIDWAY3   = 0x0810000,	// variant of UE3
 		GAME_A51,

Unreal/UnMesh3.cpp
@@ -2399,12 +2399,13 @@ struct FStaticMeshSection3
 					ps3data.unk5.Num(), ps3data.unk6.Num(), ps3data.unk7.Num(), ps3data.unk8.Num());
 			}
 		}
-		if (Ar.ArVer >= 869)
+#if GRAV
+		if (Ar.Game == GAME_GRAV && Ar.ArVer >= 869)
 		{
-			// Picked from GRAV executable
 			int32 unk;
 			Ar << unk;
 		}
+#endif // GRAV
 #if XCOM
 		if (Ar.Game == GAME_XcomB)
 		{