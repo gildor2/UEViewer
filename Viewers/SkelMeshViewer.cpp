gildor2/UEViewer

commit ad931784ab41e5475a7756df0d1b1050ea2d1d03

Fixed crash when finding UE4 animations

Viewers/SkelMeshViewer.cpp
@@ -875,7 +875,8 @@ void CSkelMeshViewer::FindUE4Animations()
 	appEnumGameFiles<TArray<const CGameFileInfo*> >( // won't compile with lambda without explicitly providing template argument
 		[](const CGameFileInfo* file, TArray<const CGameFileInfo*>& param) -> bool
 		{
-			param.Add(file);
+			if (file->Package)
+				param.Add(file);
 			return true;
 		}, PackageInfos);
 

commit b538a49fa5907b58d4ca68957d13fc975c6d7ebc

Fixed bug: "scan packages" not done anymore when finding animations
Bug was introduced in commit ad93178

Viewers/SkelMeshViewer.cpp
@@ -875,7 +875,7 @@ void CSkelMeshViewer::FindUE4Animations()
 	appEnumGameFiles<TArray<const CGameFileInfo*> >( // won't compile with lambda without explicitly providing template argument
 		[](const CGameFileInfo* file, TArray<const CGameFileInfo*>& param) -> bool
 		{
-			if (file->Package)
+			if (file->IsPackage)
 				param.Add(file);
 			return true;
 		}, PackageInfos);