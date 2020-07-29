gildor2/UEViewer

commit c532a16e5a3cf9d171b4c113a02f021d057e25f7

UE4 UTextureCube support

Unreal/UnRenderer.cpp
@@ -204,11 +204,11 @@ static void GetImageDimensions(int width, int height, int* scaledWidth, int* sca
 #endif
 
 // Decompress and upload a texture. Returns false when decompression failed.
-static bool UploadTex(UUnrealMaterial* Tex, GLenum target, CTextureData &TexData, bool doMipmap)
+static bool UploadTex(UUnrealMaterial* Tex, GLenum target, CTextureData& TexData, bool doMipmap, int slice = -1)
 {
 	guard(UploadTex);
 
-	byte *pic = TexData.Decompress(0);
+	byte *pic = TexData.Decompress(0, slice);
 	if (!pic)
 	{
 		// some internal decompression error, message should be already printed to log
@@ -278,7 +278,7 @@ static bool UploadTex(UUnrealMaterial* Tex, GLenum target, CTextureData &TexData
 		for (int mipLevel = 1; mipLevel < TexData.Mips.Num(); mipLevel++)
 		{
 			const CMipMap& Mip = TexData.Mips[mipLevel];
-			byte* pic = TexData.Decompress(mipLevel);
+			byte* pic = TexData.Decompress(mipLevel, slice);
 
 #if DEBUG_MIPS
 			// colorize mip levels
@@ -338,7 +338,7 @@ static bool UploadTex(UUnrealMaterial* Tex, GLenum target, CTextureData &TexData
 
 
 // Try to upload a compressed texture. Returns false when not uploaded (due to hardware restrictions etc).
-static bool UploadCompressedTex(UUnrealMaterial* Tex, GLenum target, GLenum target2, CTextureData &TexData, bool doMipmap)
+static bool UploadCompressedTex(UUnrealMaterial* Tex, GLenum target, GLenum target2, CTextureData& TexData, bool doMipmap, int slice = -1)
 {
 	guard(UploadCompressedTex);
 
@@ -379,9 +379,19 @@ static bool UploadCompressedTex(UUnrealMaterial* Tex, GLenum target, GLenum targ
 		format0 = GL_RGBA16F_ARB;
 		format2 = GL_HALF_FLOAT_ARB;
 	}
+
+	// Allow slices. We're supporting only 6 slices.
+	int dataSize = Mip0.DataSize;
+	const byte* dataPtr = Mip0.CompressedData;
+	if (slice >= 0)
+	{
+		dataSize /= 6;
+		dataPtr += dataSize * slice;
+	}
+
 	if (format1 && format2)
 	{
-		glTexImage2D(target, 0, format0, Mip0.USize, Mip0.VSize, 0, format1, format2, Mip0.CompressedData);
+		glTexImage2D(target, 0, format0, Mip0.USize, Mip0.VSize, 0, format1, format2, dataPtr);
 		//!! support uploading mipmaps here
 		if (doMipmap)
 			glGenerateMipmapEXT(target);
@@ -440,7 +450,7 @@ static bool UploadCompressedTex(UUnrealMaterial* Tex, GLenum target, GLenum targ
 	{
 		// no mipmaps required
 		DBG("up (%s, %d no-mips): %d %d (%d) (%s) (0x%X)", Tex->Name, TexData.Mips.Num(), Mip0.USize, Mip0.VSize, TexData.Mips.Num(), TexData.OriginalFormatName, Mip0.DataSize);
-		glCompressedTexImage2D(target, 0, format, Mip0.USize, Mip0.VSize, 0, Mip0.DataSize, Mip0.CompressedData);
+		glCompressedTexImage2D(target, 0, format, Mip0.USize, Mip0.VSize, 0, dataSize, dataPtr);
 		glTexParameteri(target2, GL_TEXTURE_MAX_LEVEL, 0);	// GL 1.2
 	}
 	else if (TexData.Mips.Num() > 1 && GL_SUPPORT(QGL_1_2)) // GL 1.2 is required for GL_TEXTURE_MAX_LEVEL
@@ -452,7 +462,16 @@ static bool UploadCompressedTex(UUnrealMaterial* Tex, GLenum target, GLenum targ
 		for (int mipLevel = 0; mipLevel < TexData.Mips.Num(); mipLevel++)
 		{
 			const CMipMap& Mip = TexData.Mips[mipLevel];
-			glCompressedTexImage2D(target, mipLevel, format, Mip.USize, Mip.VSize, 0, Mip.DataSize, Mip.CompressedData);
+			// Slices
+			dataSize = Mip.DataSize;
+			dataPtr = Mip.CompressedData;
+			if (slice >= 0)
+			{
+				dataSize /= 6;
+				dataPtr += dataSize * slice;
+			}
+			// Upload
+			glCompressedTexImage2D(target, mipLevel, format, Mip.USize, Mip.VSize, 0, dataSize, dataPtr);
 			GLenum error = glGetError();
 			DBG("   mip %d x %d (%X)", Mip.USize, Mip.VSize, Mip.DataSize);
 			if (error != 0)
@@ -464,12 +483,11 @@ static bool UploadCompressedTex(UUnrealMaterial* Tex, GLenum target, GLenum targ
 		}
 		unguard;
 	}
-	// code below generates mipmaps using OpenGL functionality
 	else if (GL_SUPPORT(QGL_EXT_FRAMEBUFFER_OBJECT))
 	{
-		// GL 3.0 or GL_EXT_framebuffer_object
+		// code below generates mipmaps using GL 3.0 or GL_EXT_framebuffer_object
 		DBG("up+build_mips (%s): %d %d (%d) (%s) (%d)", Tex->Name, Mip0.USize, Mip0.VSize, TexData.Mips.Num(), TexData.OriginalFormatName, Mip0.DataSize);
-		glCompressedTexImage2D(target, 0, format, Mip0.USize, Mip0.VSize, 0, Mip0.DataSize, Mip0.CompressedData);
+		glCompressedTexImage2D(target, 0, format, Mip0.USize, Mip0.VSize, 0, dataSize, dataPtr);
 		if (target2 != GL_TEXTURE_CUBE_MAP_ARB)
 		{
 			glGenerateMipmapEXT(target2);
@@ -480,7 +498,7 @@ static bool UploadCompressedTex(UUnrealMaterial* Tex, GLenum target, GLenum targ
 		// GL 1.4 - set GL_GENERATE_MIPMAP before uploading
 		DBG("up+build_mips (%s): old code", Tex->Name);
 		glTexParameteri(target2, GL_GENERATE_MIPMAP, GL_TRUE);
-		glCompressedTexImage2D(target, 0, format, Mip0.USize, Mip0.VSize, 0, Mip0.DataSize, Mip0.CompressedData);
+		glCompressedTexImage2D(target, 0, format, Mip0.USize, Mip0.VSize, 0, dataSize, dataPtr);
 	}
 
 	GLenum error = glGetError();
@@ -545,7 +563,7 @@ static int Upload2D(UUnrealMaterial *Tex, bool doMipmap, bool clampS, bool clamp
 }
 
 
-static bool UploadCubeSide(UUnrealMaterial *Tex, bool doMipmap, int side)
+static bool UploadCubeSide(UUnrealMaterial *Tex, int side, bool useSlices = false)
 {
 	guard(UploadCubeSide);
 
@@ -567,16 +585,18 @@ static bool UploadCubeSide(UUnrealMaterial *Tex, bool doMipmap, int side)
 	}
 #endif
 
+	int slice = useSlices ? side : 0;
+
 	// Automatic mipmap generation doesn't work with cubemaps, so allow mipmaps only for
 	// explicitly provided data.
 	// https://www.opengl.org/sdk/docs/man/html/glGenerateMipmap.xhtml
-	doMipmap = TexData.Mips.Num() > 0;
+	bool doMipmap = TexData.Mips.Num() > 1;
 
 	GLenum target = GL_TEXTURE_CUBE_MAP_POSITIVE_X_ARB + side;
-	if (!UploadCompressedTex(Tex, target, GL_TEXTURE_CUBE_MAP_ARB, TexData, doMipmap))
+	if (!UploadCompressedTex(Tex, target, GL_TEXTURE_CUBE_MAP_ARB, TexData, doMipmap, slice))
 	{
 		// upload uncompressed
-		if (!UploadTex(Tex, target, TexData, doMipmap))
+		if (!UploadTex(Tex, target, TexData, doMipmap, slice))
 		{
 			// some internal decompression error, message should be already displayed
 			return false;
@@ -1285,7 +1305,7 @@ bool UCubemap::Upload()
 	for (int side = 0; side < 6; side++)
 	{
 		UTexture *Tex = Faces[side];
-		bool success = UploadCubeSide(Tex, Tex->Mips.Num() > 1, side);
+		bool success = UploadCubeSide(Tex, side);
 		// release bulk data immediately
 		//!! better - use Lock (when uploaded)/Unlock (when destroyed/released) for referenced textures
 		Tex->ReleaseTextureData();
@@ -2171,7 +2191,7 @@ bool UTextureCube3::Upload()
 			break;
 		}
 
-		bool success = UploadCubeSide(Tex, Tex->Mips.Num() > 1, side);
+		bool success = UploadCubeSide(Tex, side);
 		// release bulk data immediately
 		//!! better - use Lock (when uploaded)/Unlock (when destroyed/released) for referenced textures
 		Tex->ReleaseTextureData();
@@ -2227,6 +2247,79 @@ void UTextureCube3::Release()
 	unguard;
 }
 
+
+#if UNREAL4
+
+
+bool UTextureCube4::Upload()
+{
+	if (NumSlices != 6)
+		return Super::Upload(); // use as Texture2D
+
+	if (TexNum == BAD_TEXTURE) return false;
+	if (GL_TouchObject(DrawTimestamp)) return true;
+
+	// upload all cube sides
+	glGenTextures(1, &TexNum);
+	glBindTexture(GL_TEXTURE_CUBE_MAP_ARB, TexNum);
+	for (int side = 0; side < 6; side++)
+	{
+		bool success = UploadCubeSide(this, side, true);
+		// release bulk data immediately
+
+		if (!success)
+		{
+			glDeleteTextures(1, &TexNum);
+			TexNum = BAD_TEXTURE;
+			break;
+		}
+	}
+
+	ReleaseTextureData();
+
+	return (TexNum != BAD_TEXTURE);
+}
+
+bool UTextureCube4::Bind()
+{
+	guard(UTextureCube4::Bind);
+
+	if (NumSlices != 6) // probably source texture
+		return Super::Bind();
+
+	glEnable(GL_TEXTURE_CUBE_MAP_ARB);
+
+	if (!Upload())
+	{
+		glDisable(GL_TEXTURE_CUBE_MAP_ARB);
+		return false;
+	}
+
+	// bind texture
+	glBindTexture(GL_TEXTURE_CUBE_MAP_ARB, TexNum);
+	return true;
+
+	unguard;
+}
+
+
+void UTextureCube4::GetParams(CMaterialParams &Params) const
+{
+	if (NumSlices != 6) return; // can't show this as cubemap
+	Params.Cube = (UUnrealMaterial*)this;
+}
+
+
+void UTextureCube4::Release()
+{
+	guard(UTextureCube4::Release);
+	if (GL_IsValidObject(TexNum, DrawTimestamp))
+		glDeleteTextures(1, &TexNum);
+	Super::Release();
+	unguard;
+}
+
+#endif // UNREAL4
 
 void UMaterialInstanceConstant::SetupGL()
 {

Unreal/UnrealMaterial/UnMaterial.h
@@ -257,7 +257,7 @@ public:
 		return true;
 	}
 
-	byte* Decompress(int MipLevel = 0);				// may return NULL in a case of error
+	byte* Decompress(int MipLevel = 0, int Slice = -1);		// may return NULL in a case of error
 
 #if SUPPORT_XBOX360
 	bool DecodeXBox360(int MipLevel);

Unreal/UnrealMaterial/UnMaterial3.h
@@ -160,7 +160,9 @@ public:
 	float			UnpackMin[4];
 	float			UnpackMax[4];
 	FByteBulkData	SourceArt;
+#if UNREAL4
 	FTextureSource	Source;
+#endif
 	ETextureCompressionSettings CompressionSettings;
 
 	UTexture3()
@@ -426,6 +428,7 @@ public:
 	bool			bForcePVRTC4;		// iPhone
 #if UNREAL4
 	FIntPoint		ImportedSize;
+	int				NumSlices;			//todo: important only while UTextureCube4 is derived from UTexture2D in out implementation
 #endif
 
 #if RENDERING
@@ -440,6 +443,7 @@ public:
 	,	AddressX(TA_Wrap)
 	,	AddressY(TA_Wrap)
 	,	bForcePVRTC4(false)
+	,	NumSlices(1)
 	{}
 #endif
 
@@ -513,12 +517,12 @@ class UTextureCube3 : public UTexture3
 {
 	DECLARE_CLASS(UTextureCube3, UTexture3)
 public:
-	UTexture2D		*FacePosX;
-	UTexture2D		*FaceNegX;
-	UTexture2D		*FacePosY;
-	UTexture2D		*FaceNegY;
-	UTexture2D		*FacePosZ;
-	UTexture2D		*FaceNegZ;
+	UTexture2D*		FacePosX;
+	UTexture2D*		FaceNegX;
+	UTexture2D*		FacePosY;
+	UTexture2D*		FaceNegY;
+	UTexture2D*		FacePosZ;
+	UTexture2D*		FaceNegZ;
 
 #if RENDERING
 	// rendering implementation fields
@@ -561,6 +565,29 @@ public:
 	}
 #endif // RENDERING
 };
+
+
+#if UNREAL4
+
+// In UE4, UTextureCube is derived from UTexture, however it has almost identical serializer to UTexture2D.
+// In order to reuse the code, we'll derive it from UTexture2D.
+class UTextureCube4 : public UTexture2D
+{
+	DECLARE_CLASS(UTextureCube4, UTexture2D)
+public:
+#if RENDERING
+	virtual bool Upload();
+	virtual bool Bind();
+	virtual void GetParams(CMaterialParams &Params) const;
+	virtual void Release();
+	virtual bool IsTextureCube() const
+	{
+		return true;
+	}
+#endif // RENDERING
+};
+
+#endif // UNREAL4
 
 
 enum EBlendMode
@@ -1228,7 +1255,7 @@ public:
 	REGISTER_ENUM(EMobileSpecularMask)	\
 
 #define REGISTER_MATERIAL_CLASSES_U4	\
-	REGISTER_CLASS_ALIAS(UTexture2D, UTextureCube) \
+	REGISTER_CLASS_ALIAS(UTextureCube4, UTextureCube) \
 	REGISTER_CLASS(FMaterialParameterInfo) \
 	REGISTER_CLASS(FMaterialCachedParameterEntry) \
 	REGISTER_CLASS(FMaterialCachedParameters) \

Unreal/UnrealMaterial/UnTexture.cpp
@@ -97,7 +97,7 @@ unsigned CTextureData::GetFourCC() const
 }
 
 
-byte* CTextureData::Decompress(int MipLevel)
+byte* CTextureData::Decompress(int MipLevel, int Slice)
 {
 	guard(CTextureData::Decompress);
 
@@ -110,6 +110,12 @@ byte* CTextureData::Decompress(int MipLevel)
 	int USize = Mip.USize;
 	int VSize = Mip.VSize;
 	const byte *Data = Mip.CompressedData;
+
+	if (Slice >= 0)
+	{
+		// 6 slices in UE4 follows each other in memory
+		Data += Mip.DataSize / 6 * Slice;
+	}
 
 	int pixelSize = PixelFormatInfo[Format].Float ? 16 : 4;
 	int size = USize * VSize * pixelSize;

Unreal/UnrealMaterial/UnTexture4.cpp
@@ -212,6 +212,7 @@ void UTexture2D::Serialize4(FArchive& Ar)
 				SizeX = Data.SizeX;
 				SizeY = Data.SizeY;
 				Format = PixelFormat;
+				NumSlices = Data.NumSlices;
 			}
 			else
 			{

commit f5bbe9fd2215359643ecf0ab8b7ac8b38eecd4bd

Fixed Cubemap upload
Bug introduced in c532a16e5a3cf9d171b4c113a02f021d057e25f7
Also replaced appNotify() error messages about failed upload with appPrintf()

Unreal/UnRenderer.cpp
@@ -480,7 +480,7 @@ static bool UploadCompressedTex(UUnrealMaterial* Tex, GLenum target, GLenum targ
 			DBG("   mip %d x %d (%X)", Mip.USize, Mip.VSize, Mip.DataSize);
 			if (error != 0)
 			{
-				appNotify("Failed to upload mip %d of texture %s in format 0x%04X: error 0x%X\n", mipLevel, Tex->Name, format, error);
+				appPrintf("Failed to upload mip %d of texture %s in format 0x%04X: error 0x%X\n", mipLevel, Tex->Name, format, error);
 				DBG("%d x %d (%X)", Mip.USize, Mip.VSize, Mip.DataSize);
 				break;
 			}
@@ -508,7 +508,7 @@ static bool UploadCompressedTex(UUnrealMaterial* Tex, GLenum target, GLenum targ
 	GLenum error = glGetError();
 	if (error)
 	{
-		appNotify("Failed to upload texture %s in format 0x%04X, error 0x%04X", Tex->Name, format, error);
+		appPrintf("Failed to upload texture %s in format 0x%04X, error 0x%04X\n", Tex->Name, format, error);
 		return false;
 	}
 
@@ -589,7 +589,7 @@ static bool UploadCubeSide(UUnrealMaterial *Tex, int side, bool useSlices = fals
 	}
 #endif
 
-	int slice = useSlices ? side : 0;
+	int slice = useSlices ? side : -1;
 
 	// Automatic mipmap generation doesn't work with cubemaps, so allow mipmaps only for
 	// explicitly provided data.