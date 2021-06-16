63f6f28e46989ea87afdc5d0a5e9ec92dbac349f
adludum99 <60808025+adludum99@users.noreply.github.com>
2021-06-17 07:44:08 +0800

    Sync 1558

--- Unreal/UnrealMaterial/UnMaterial3.h
+++ Unreal/UnrealMaterial/UnMaterial3.h
@@ -1163,6 +1163,39 @@ struct FVectorParameterValue
 	END_PROP_TABLE
 };
 
+#if UNREAL4
+
+struct FRuntimeVirtualTextureParameterValue
+{
+	DECLARE_STRUCT(FRuntimeVirtualTextureParameterValue)
+
+//	FGuid			ExpressionGUID;
+	FMaterialParameterInfo ParameterInfo;
+
+	BEGIN_PROP_TABLE
+		PROP_STRUC(ParameterInfo, FMaterialParameterInfo) // property 0
+		PROP_DROP(ParameterValue, PropType::UObject)	// property 1
+		PROP_DROP(ExpressionGUID, PropType::FVector4)	// property 2
+	END_PROP_TABLE
+};
+
+struct FFontParameterValue
+{
+	DECLARE_STRUCT(FFontParameterValue)
+
+//	FGuid			ExpressionGUID;
+	FMaterialParameterInfo ParameterInfo;
+
+	BEGIN_PROP_TABLE
+		PROP_STRUC(ParameterInfo, FMaterialParameterInfo) // property 0
+		PROP_DROP(FontValue, PropType::UObject)			// property 1
+		PROP_DROP(FontPage, PropType::Int)				// property 2
+		PROP_DROP(ExpressionGUID, PropType::FVector4)	// property 3
+	END_PROP_TABLE
+};
+
+#endif // UNREAL4
+
 class UMaterialInstance : public UMaterialInterface
 {
 	DECLARE_CLASS(UMaterialInstance, UMaterialInterface)
@@ -1174,6 +1207,8 @@ public:
 #if UNREAL4
 	FMaterialInstanceBasePropertyOverrides BasePropertyOverrides;
 	FStaticParameterSet StaticParameters;
+	TArray<FRuntimeVirtualTextureParameterValue> RuntimeVirtualTextureParameterValues;
+	TArray<FFontParameterValue> FontParameterValues;
 #endif
 
 	BEGIN_PROP_TABLE
@@ -1185,6 +1220,8 @@ public:
 #if UNREAL4
 		PROP_STRUC(BasePropertyOverrides, FMaterialInstanceBasePropertyOverrides)
 		PROP_STRUC(StaticParameters, FStaticParameterSet)
+		PROP_ARRAY(RuntimeVirtualTextureParameterValues, "FRuntimeVirtualTextureParameterValue")
+		PROP_ARRAY(FontParameterValues, "FFontParameterValue")
 #endif
 		PROP_DROP(PhysMaterial)
 		PROP_DROP(bHasStaticPermutationResource)
@@ -1308,7 +1345,9 @@ public:
 	REGISTER_CLASS(FStaticMaterialLayersParameter) \
 	REGISTER_CLASS(FStaticParameterSet) \
 	REGISTER_CLASS(FLightmassMaterialInterfaceSettings) \
-	REGISTER_CLASS(FMaterialTextureInfo)
+	REGISTER_CLASS(FMaterialTextureInfo) \
+	REGISTER_CLASS(FFontParameterValue) \
+	REGISTER_CLASS(FRuntimeVirtualTextureParameterValue)
 
 #define REGISTER_MATERIAL_ENUMS_U4		\
 	REGISTER_ENUM(ETextureSourceFormat)
